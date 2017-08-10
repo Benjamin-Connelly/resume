c2c0b09e342fa55477f40ba9b94f319b0e96caa5
Benjamin Connelly <benjamin.t.connelly@gmail.com>
2017-08-10 17:33:17 -0500

    remote .tex file

--- pandoc_source/convert.sh
+++ pandoc_source/convert.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-#Update Benjamin_Connelly_Resume.md then convert to .tex
-pandoc Resume_Template.md -f markdown -t latex -s -o Benjamin_Connelly_Resume.tex
-
 # Create .md .html and .pdf versions
 pandoc Resume_Template.md -o ../Benjamin_Connelly_Resume.md
 pandoc Resume_Template.md -f markdown -t html -s -o ../Benjamin_Connelly_Resume.html
