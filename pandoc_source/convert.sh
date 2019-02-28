b54dc1139ea706809e909ac4440ab7b9e98a6ce5
Benjamin Connelly <benjamin.t.connelly@gmail.com>
2019-02-27 16:30:43 -0800

    added docx

--- pandoc_source/convert.sh
+++ pandoc_source/convert.sh
@@ -4,3 +4,5 @@
 pandoc Resume_Template.md -o ../Benjamin_Connelly_Resume.md
 pandoc Resume_Template.md -f markdown -t html -s -o ../Benjamin_Connelly_Resume.html
 pandoc Resume_Template.md -o ../Benjamin_Connelly_Resume.pdf
+pandoc Resume_Template.md -o ../Benjamin_Connelly_Resume.docx
+
